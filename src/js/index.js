bcbc3e0bee6d59a51c44f69073066b55da34b01b
ivan52945 <ivan52945@gmail.com>
2022-08-25 01:27:44 +0300

    feat: add raw realization

--- src/js/index.js
+++ src/js/index.js
@@ -2,37 +2,59 @@ import ancientsData from "../../data/ancients.js";
 import difficulties from "../../data/difficulties.js";
 import { brownCards, blueCards, greenCards } from "../../data/mythicCards/index.js";
 
+const cardList = {
+	green: {
+		easy: [],
+		normal: [],
+		hard: [],
+	},
+	brown: {
+		easy: [],
+		normal: [],
+		hard: [],
+	},
+	blue: {
+		easy: [],
+		normal: [],
+		hard: [],
+	},
+};
+
+let cards = [...brownCards, ...blueCards, ...greenCards];
+
+cards.forEach(function (card) {
+	cardList[card.color][card.difficulty].push(card);
+});
+
+
+
 const ancientField = document.querySelector('.ancient-field');
 
-ancientsData.forEach((element, id) => { 
+ancientsData.forEach((element, id) => {
 	const card = new Image();
 	card.src = element.cardFace;
 	card.dataset.id = id;
 	card.classList.add("ancient-field__ancient-card");
-	card.onload = function () { ancientField.innerHTML += card.outerHTML};
+	card.onload = function () { ancientField.innerHTML += card.outerHTML };
 });
 
-let activeAncient = null;
+let activeAncientTag = null;
 
 ancientField.addEventListener("click", function (event) {
-	if (event.target.tagName == "IMG") { 
-		activeAncient?.classList.remove("active");
-		activeAncient = event.target;
-		activeAncient.classList.add("active");
+	if (event.target.tagName == "IMG") {
+		activeAncientTag?.classList.remove("active");
+		activeAncientTag = event.target;
+		activeAncientTag.classList.add("active");
 	}
 });
 
-
-
-
-
 const levelList = document.querySelector('.level');
 
-difficulties.forEach((element, id) => { 
+difficulties.forEach((element) => {
 	const levelTitle = document.createElement("li");
 	levelTitle.textContent = element.name;
 	levelTitle.classList.add("level__name");
-	levelTitle.dataset.id = id;
+	levelTitle.dataset.id = element.id;
 	levelList.innerHTML += levelTitle.outerHTML;
 });
 
@@ -45,3 +67,47 @@ levelList.addEventListener("click", function (event) {
 		activeLevel.classList.add("active");
 	}
 });
+
+function randomInteger(min, max) {
+	let rand = min - 0.5 + Math.random() * (max - min + 1);
+	return Math.round(rand);
+}
+
+
+
+const button = document.querySelector(".button");
+
+button.addEventListener("click", function () {
+	if (!activeAncientTag || !activeLevel) { return; }
+
+	const activeAncient = ancientsData[activeAncientTag.dataset.id];
+
+	const nedeedCard = {};
+
+	for (let key in cardList) {
+		nedeedCard[key] = activeAncient.stages.reduce((acc, numCards) => { return acc += numCards[key] }, 0);
+	}
+
+	const outputCardList = {};
+
+	if (activeLevel.dataset.id == "very easy") {
+		for (let key in cardList) {
+			outputCardList[key] = [...cardList[key].easy];
+			while (outputCardList[key].length > nedeedCard[key]) {
+				outputCardList[key].splice(randomInteger(0, outputCardList[key].length - 1), 1);
+			}
+
+			if (outputCardList[key].length < nedeedCard[key]) {
+				const addCard = [...cardList[key].normal];
+				while (addCard.length > (nedeedCard[key] - outputCardList[key].length)) {
+					addCard.splice(randomInteger(0, addCard.length - 1), 1);
+				}
+				outputCardList[key] = [...outputCardList[key], ...addCard];
+			}
+		}
+		console.log(outputCardList);
+	}
+
+});
+
+// npx webpack --config webpack.config.js
