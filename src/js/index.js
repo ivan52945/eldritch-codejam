c62c24b4722b37498f3d97bd5a56fbedc3322656
ivan52945 <ivan52945@gmail.com>
2022-08-25 21:38:29 +0300

    feat: finish realization width very easy level

--- src/js/index.js
+++ src/js/index.js
@@ -4,36 +4,12 @@ import { brownCards, blueCards, greenCards } from "../../data/mythicCards/index.
 
 var _ = require('lodash');
 
-const cardList = {
-	green: {
-		easy: [],
-		normal: [],
-		hard: [],
-	},
-	brown: {
-		easy: [],
-		normal: [],
-		hard: [],
-	},
-	blue: {
-		easy: [],
-		normal: [],
-		hard: [],
-	},
-};
-
 const cardNumberOutputDiv = document.querySelectorAll('.mysthic-card-field__card-counter ');
 const cardNumberOutput = {};
-cardNumberOutputDiv.forEach(function (cardCount) { 
+cardNumberOutputDiv.forEach(function (cardCount) {
 	cardNumberOutput[`${cardCount.dataset.id} ${cardCount.dataset.color}`] = cardCount;
 	cardCount.textContent = 0;
 })
-
-let cards = _.cloneDeep([...brownCards, ...blueCards, ...greenCards]);
-
-cards.forEach(function (card) {
-	cardList[card.color][card.difficulty].push(card);
-});
 
 const ancientField = document.querySelector('.ancient-field');
 
@@ -63,6 +39,8 @@ difficulties.forEach((element) => {
 	levelList.innerHTML += levelTitle.outerHTML;
 });
 
+const cardList = _.cloneDeep({ green: greenCards, brown: brownCards, blue: blueCards });
+
 let activeLevel = null;
 levelList.addEventListener("click", function selectLevel(event) {
 	if (event.target.tagName == "LI") {
@@ -86,26 +64,95 @@ function shuffle(array) {
 
 const button = document.querySelector(".button");
 
-let outputCardStageList = [];
+let stageColodaOut = null;
 let stage = 0;
 let cardIndex = 0;
 
 const cardClosed = document.querySelector(".card-closed");
 const cardOpened = document.querySelector(".card-opened");
 
-button.addEventListener("click", function () {
+button.addEventListener("click", function createColoda() {
 	if (!activeAncientTag || !activeLevel) { return; }
 
 	const activeAncient = _.cloneDeep(ancientsData[activeAncientTag.dataset.id]);
 
-	activeAncient.stages.forEach(function (stage, index) { 
-		for (let color in stage) { 
+	activeAncient.stages.forEach(function (stage, index) {
+		for (let color in stage) {
 			cardNumberOutput[`${index} ${color}`].textContent = stage[color];
 		}
 	});
 
-	const nedeedCard = {};
+	const cardListClone = _.cloneDeep(cardList);
 
+	for (let color in cardListClone) {
+		for (let dificulty in cardListClone[color]) {
+			shuffle(cardListClone[color][dificulty]);
+		}
+	}
+
+	stageColodaOut = [];
+
+	if (activeLevel.dataset.id == "very easy") {
+		for (let stage in activeAncient.stages) {
+			stageColodaOut[stage] = [];
+			for (let color in activeAncient.stages[stage]) {
+
+				const neededCardNumber = activeAncient.stages[stage][color];
+
+				const stageColorList = cardListClone[color].easy.splice(0, neededCardNumber);
+
+				if (stageColorList.length < neededCardNumber) {
+					stageColorList.splice(-1, 0, ...cardListClone[color].normal.splice(0, neededCardNumber - stageColorList.length));
+				}
+				stageColodaOut[stage].splice(-1, 0, ...stageColorList);
+				shuffle(stageColodaOut[stage]);
+			}
+		}
+	} else if (activeLevel.dataset.id == "easy") {
+		for (let stage in activeAncient.stages) {
+			stageColodaOut[stage] = [];
+			for (let color in activeAncient.stages[stage]) {
+
+				const neededCardNumber = activeAncient.stages[stage][color];
+
+				const stageColorList = cardListClone[color].easy.splice(0, neededCardNumber);
+
+				if (stageColorList.length < neededCardNumber) {
+					stageColorList.splice(-1, 0, ...cardListClone[color].normal.splice(0, neededCardNumber - stageColorList.length));
+				}
+				stageColodaOut[stage].splice(-1, 0, ...stageColorList);
+				shuffle(stageColodaOut[stage]);
+			}
+		}
+	}
+
+	stage = 0;
+	cardIndex = 0;
+	cardOpened.hidden = true;
+	cardClosed.hidden = false;
+});
+
+
+
+cardClosed.addEventListener("click", function () {
+	cardOpened.hidden = false;
+
+	cardOpened.src = stageColodaOut[stage][cardIndex].cardFace;
+	cardNumberOutput[`${stage} ${stageColodaOut[stage][cardIndex].color}`].textContent = +cardNumberOutput[`${stage} ${stageColodaOut[stage][cardIndex].color}`].textContent - 1;
+
+	cardIndex++;
+
+	if (cardIndex >= stageColodaOut[stage].length) { stage++; cardIndex = 0; }
+	if (stage >= stageColodaOut.length) { cardClosed.hidden = true; }
+});
+
+
+// npx webpack --config webpack.config.js
+
+/*
+
+
+	const nedeedCard = {};
 	for (let key in cardList) {
 		nedeedCard[key] = activeAncient.stages.reduce((acc, numCards) => { return acc += numCards[key] }, 0);
 	}
@@ -132,9 +179,9 @@ button.addEventListener("click", function () {
 
 	const outputCardStageListIn = []
 
-	activeAncient.stages.forEach(function (colorList, stage) { 
+	activeAncient.stages.forEach(function (colorList, stage) {
 		outputCardStageListIn[stage] = [];
-		for (let color in colorList) { 
+		for (let color in colorList) {
 			for (let i = 0; i < colorList[color]; i++) {
 				outputCardStageListIn[stage].push(outputCardList[color].splice(randomInteger(0, outputCardList[color].length - 1), 1)[0]);
 			}
@@ -142,28 +189,4 @@ button.addEventListener("click", function () {
 		}
 
 	});
-
-	outputCardStageList = outputCardStageListIn;
-	stage = 0;
-	cardIndex = 0;
-	cardOpened.hidden = true;
-	cardClosed.hidden = false;
-	console.log(outputCardStageList);
-});
-
-
-
-cardClosed.addEventListener("click", function () { 
-	console.log(stage,cardIndex);
-
-	cardOpened.src = outputCardStageList[stage][cardIndex].cardFace;
-	cardNumberOutput[`${stage} ${outputCardStageList[stage][cardIndex].color}`].textContent = +cardNumberOutput[`${stage} ${outputCardStageList[stage][cardIndex].color}`].textContent - 1;
-	cardOpened.hidden = false;
-	cardIndex++;
-
-	if (cardIndex >= outputCardStageList[stage].length) { stage++; cardIndex = 0; }
-	if (stage >= outputCardStageList.length) { cardClosed.hidden = true; }
-});
-
-
-// npx webpack --config webpack.config.js
+	*/
