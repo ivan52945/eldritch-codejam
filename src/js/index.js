2b0b9e062e8ba188f474e368454d1352e79015fa
unknown <sysadmin@4-505-5.bsuir.by>
2022-08-25 15:30:01 +0300

    refactor: start add cloning

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import ancientsData from "../../data/ancients.js";
 import difficulties from "../../data/difficulties.js";
 import { brownCards, blueCards, greenCards } from "../../data/mythicCards/index.js";
 
+var _ = require('lodash');
+
 const cardList = {
 	green: {
 		easy: [],
@@ -21,20 +23,17 @@ const cardList = {
 };
 
 const cardNumberOutputDiv = document.querySelectorAll('.mysthic-card-field__card-counter ');
-
 const cardNumberOutput = {};
-
 cardNumberOutputDiv.forEach(function (cardCount) { 
 	cardNumberOutput[`${cardCount.dataset.id} ${cardCount.dataset.color}`] = cardCount;
 	cardCount.textContent = 0;
 })
 
-let cards = [...brownCards, ...blueCards, ...greenCards];
+let cards = _.cloneDeep([...brownCards, ...blueCards, ...greenCards]);
 
 cards.forEach(function (card) {
 	cardList[card.color][card.difficulty].push(card);
 });
-
 
 const ancientField = document.querySelector('.ancient-field');
 
@@ -94,15 +93,13 @@ let outputCardStageList = [];
 let stage = 0;
 let cardIndex = 0;
 
-const card = document.querySelector(".mysthic-card-field__card-field");
-
 const cardClosed = document.querySelector(".card-closed");
 const cardOpened = document.querySelector(".card-opened");
 
 button.addEventListener("click", function () {
 	if (!activeAncientTag || !activeLevel) { return; }
 
-	const activeAncient = ancientsData[activeAncientTag.dataset.id];
+	const activeAncient = _.cloneDeep(ancientsData[activeAncientTag.dataset.id]);
 
 	activeAncient.stages.forEach(function (stage, index) { 
 		for (let color in stage) { 
